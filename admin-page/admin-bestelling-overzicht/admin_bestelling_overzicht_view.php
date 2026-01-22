83827485b0d3d5e2c14ba44121fcecae4e34397c
101756 <101756@glr.nl>
2026-01-22 11:20:29 +0100

    header css full kan nog aangepast worden

--- admin-page/admin-bestelling-overzicht/admin_bestelling_overzicht_view.php
+++ admin-page/admin-bestelling-overzicht/admin_bestelling_overzicht_view.php
@@ -18,6 +18,21 @@ $stmt->execute();
 $resultaten = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+<link rel="stylesheet" href="../../Header.css">
+
+<header>
+    <!-- Logo links -->
+    <div class="logo">
+        <img src="../../image/Logo.png" alt="Logo" height="50">
+    </div>
+
+    <!-- Knoppen rechts -->
+    <nav class="nav-buttons">
+        <a href="recepten.html" class="header-btn">Recepten</a>
+        <a href="index.html" class="header-btn">Home</a>
+        <a href="bestel.html" class="header-btn">Bestel</a>
+    </nav>
+</header>
 <h1>Geplaatste orders</h1>
 
 <?php if (count($resultaten) > 0): ?>
