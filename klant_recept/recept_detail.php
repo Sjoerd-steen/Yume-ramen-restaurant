bbdfe2991b3a51f25ff8a51f19e99a95a195523e
kayleigh-101759 <101759@glr.nl>
2026-01-23 13:25:30 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ klant_recept/recept_detail.php
@@ -0,0 +1,32 @@
+<?php
+require './config.php';
+
+
+//lees het ID uit de URL
+$ID = $_GET['ID'];
+//toon het id op het scherm
+echo "ID: " . $ID . "
+";
+
+try {
+    $query = "SELECT * FROM MenuItem where ID = :ID";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':ID', $ID);
+    $stmt->execute();
+
+//haal alle resultaten op in een array
+    $resultaten = $stmt->fetchAll();
+
+//aantl resultaten tellen
+    $aantalRijen = count($resultaten);
+
+
+    include 'view/recept_view.php';
+
+} catch (PDOException $e) {
+    // foutafhandeling als de quert niet wordt uitgevoerd
+    echo "<p>Fout!</p>";
+    echo "<p>Query: " . $query . "</p>";
+    echo "<p>Foutmelding: " . $e->getMessage() . "</p>";
+    exit;
+};
---
+++ klant_recept/view/recept_view.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/agenda_view.css">
+</head>
+<body>
+<h1>Recepten - Klant</h1>
+
+<?php
+if ($aantalRijen > 0) { ?>
+    <ul>
+        <?php foreach ($resultaten as $rij) { ?>
+            <li>
+                <p><strong>Naam:</strong> <?= htmlspecialchars($rij["Naam"]) ?></p>
+                <p><strong>Beschrijving:</strong> <?= htmlspecialchars($rij["Beschrijving"]) ?></p>
+                <p><strong>Categorie:</strong> <?= htmlspecialchars($rij["Category"]) ?></p>
+                <p><strong>foto:</strong> <?= htmlspecialchars($rij["ImageURL"]) ?></p>
+                <p><strong>Prijs:</strong> <?= htmlspecialchars($rij["Price"]) ?></p>
+
+                <strong>Acties</strong>
+                <a href="recept.php?ID<?=$rij['ID']?>">overzicht</a>
+                <a href="recept_detail.php?ID=<?=$rij['ID']?>">Details</a>
+            </li>
+            <hr>
+        <?php } ?>
+    </ul>
+<?php } else { ?>
+    <p>Geen resultaten gevonden</p>
+<?php } ?>
+<a href="recept_toevoegen_verwerk.php" class="knop-toevoegen">toevoegen</a>
+</body>
+</html>
